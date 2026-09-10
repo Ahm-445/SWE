2643b1d6094aec25af28a65599b41fe345b5a5d0
Ahmed Alqahtani <ahmedswe445@gmail.com>
2026-09-11 02:18:17 +0300

    Add Transition time 0.5s

--- SWE_frontEnd/src/App.jsx
+++ SWE_frontEnd/src/App.jsx
@@ -1,7 +1,7 @@
 import Home from "./Pages/Home"
 import Navbar from "./Pages/Navbar"
 import GPA from "./Pages/GPA"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import News from "./Pages/News";
 import Evaluation from "./Pages/Evaluation";
 import Absence from "./Pages/Absence";
@@ -9,13 +9,12 @@ import Table from "./Pages/table";
 import NotFound404 from "./Pages/NotFound404";
 
 
-function App() {
-  return (
-    <div className="flex flex-col h-screen overflow-auto dir-rtl">
-      
+function AnimatedRoutes() {
+  const location = useLocation(); 
 
-  <BrowserRouter>
-      <Routes>
+  return (
+    <div key={location.pathname} className="page-animate flex-1">
+      <Routes location={location}>
         <Route path="/" element={<><Navbar/><Home /></>} />
         <Route path="/news" element={<><Navbar/><News /></>} />
         <Route path="/gpa" element={<><Navbar/><GPA /></>} />
@@ -24,8 +23,17 @@ function App() {
         <Route path="/schedule" element={<><Navbar/><Table /></>} />
         <Route path="*" element={<><Navbar/><NotFound404 /></>} />
       </Routes>
-    </BrowserRouter>
+    </div>
+  );
+}
+  
 
+function App() {
+  return (
+    <div className="flex flex-col h-screen overflow-auto dir-rtl">
+      <BrowserRouter>
+        <AnimatedRoutes />
+      </BrowserRouter>
     </div>
   )
 }
